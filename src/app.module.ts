b34f99da372ef1539ef6125caadf35eb1184c831
unknown <checkpoint1209@gmail.com>
2021-06-05 12:49:39 +0900

    make it better

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,11 +15,12 @@ import { Post } from './post/entities/post.entity';
     GraphQLModule.forRoot({ autoSchemaFile: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: 'ec2-34-206-8-52.compute-1.amazonaws.com',
       port: 5432,
-      username: 'postgres',
-      password: '1234',
-      database: 'trablog',
+      username: 'czkabudzqpkvto',
+      password:
+        '0ff78c554e8f295d55a73cae6cbf90ecfc94b4987aeaf38d2885929bbe1bb098',
+      database: 'd8mca5gl44d76r',
       entities: [User, Post],
       synchronize: true,
     }),
