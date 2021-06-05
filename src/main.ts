98de09ef5db121cbd33f6a121338cc44e38565f9
unknown <checkpoint1209@gmail.com>
2021-06-05 13:05:03 +0900

    make it better

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(443);
+  await app.listen(80);
 }
 bootstrap();
 
